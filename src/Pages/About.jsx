b4cf6d6d9fec8317399df5d08c4ef9292e80bb1f
Kamogelo Mkwanazi <kamopmkwanazi@gmail.com>
2024-08-30 13:23:01 +0200

    mnn

--- src/Pages/About.jsx
+++ src/Pages/About.jsx
@@ -10,15 +10,14 @@ export const About = () => {
     <div>
         <div className='relative h-screen bg-cover bg-center' 
             style={{backgroundImage: `url(${backgroundPic})`}}>
-            <div className='md:container mx-auto px-4'>
-                <div>
-                    <h3 className='text-center pt-28 mb-10 text-2xl md:text-4xl text-white'>About Me</h3>
+            <div className='container mx-auto px-4'>
+                 <h3 className='text-center pt-28 mb-10 text-2xl md:text-4xl text-white'>About Me</h3>
                     <p className='text-white text-lg md:text-xl'>My name is Kamogelo Patience Mkwanazi, a passionate and dedicated Computer Systems Engineer with a love for web development.
                         My journey began at the Tshwane University of Technology where I discovered my love for technology. Since then, I've had an
                         opportunity to work on various projects. Currently I'm focused on web development, mainly front-end development.
                         I'm always open to new opportunities and collaborations.  
                     </p>
-                </div>
+                
                 <div>
                     <h3 className='text-center pt-20 mb-10 text-2xl md:text-4xl text-white'>My Skills</h3>
                     <div className="flex flex-col sm:flex-row sm:flex-wrap sm:justify-between">
