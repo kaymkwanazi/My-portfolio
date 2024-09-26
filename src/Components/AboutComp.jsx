85c0b2eec06e6461529b12281876a9db653b00ed
Kamogelo Mkwanazi <kamopmkwanazi@gmail.com>
2024-09-26 14:05:14 +0200

    small changes

--- src/Components/AboutComp.jsx
+++ src/Components/AboutComp.jsx
@@ -10,9 +10,10 @@ export const AboutComp = () => {
     <>
         <div className='grid cols-1 md:grid-cols-2 text-white text-center'>
             <div className='relative flex justify-center items-center min-h-screen'>
-                <img src={Kamo2} className='relative z-10 w-60 mb-10'></img> 
+                <img src={Kamo2} className='relative z-10 w-60'></img> 
              {/* circular path for icons */}
                 <div className='absolute w-[400px] h-[400px] flex justify-center items-center border border-dashed border-pink-600 rounded-full'>
+                   {/* Rotating icons */}
                     <div className='absolute w-full h-full animate-rotate-icons'>
                         <FaReact size={46} className='absolute top-0 left-1/2 transform -translate-x-1/2 -translate-y-1/2 text-blue-500 bg-white px-2 rounded-full'/>
                         <FaNodeJs size={46} className='absolute right-0 top-1/2 transform translate-x-5 -translate-y-1/2 text-teal-500 bg-white px-2 rounded-full'/>
@@ -24,10 +25,10 @@ export const AboutComp = () => {
             </div>
             <div className='flex flex-col justify-center items-center'>
                 <div className='bg-gradient-to-b from-darkPurple via-purple-500 to-lightPurple bg-clip-text text-transparent'>
-                    <h3 className='md:text-3xl font-bold mb-8'>ABOUT ME</h3>
+                    <h3 className='text-2xl md:text-3xl font-bold mb-8'>ABOUT ME</h3>
                 </div>
                 <p className='text-2xl md:text-4xl mb-3'>Always Striving for Excellence</p>
-                <p className='text-xl mb-16'>With strong interpersonal-skills, clear communication and meticulous attention to detail, 
+                <p className='md:text-xl mb-16'>With strong interpersonal-skills, clear communication and meticulous attention to detail, 
                 I am committed to delivering exceptional results in every project I take on.</p>
                 <div className='bg-gradient-to-r from-darkPurple to-lightPurple rounded-full'>
                     <Link to= '/about' className='flex justify-center'>
--- src/Pages/Home.jsx
+++ src/Pages/Home.jsx
@@ -12,15 +12,15 @@ export const Home = () => {
     <>
     <div className='relative min-h-screen bg-cover bg-center'
         style={{backgroundImage: `url(${backgroundPic})`}}>
-        <div className='flex flex-col justify-center items-center min-h-screen pb-20'>
+        <div className='flex flex-col justify-center items-center min-h-screen'>
             <div className='container mx-auto px-4'>
                 <div className='grid cols-1 md:grid-cols-2'>
                     <div className='flex justify-center'>
                         <img src={kamo} className='w-1/2 h-3/4 mt-24 rounded-3xl'></img>
                     </div>    
                     <div className='flex flex-col justify-center text-center'> 
-                        <h1 className='text-white text-2xl md:text-5xl '><MdWavingHand size={52} className='inline text-yellow-400'/> Hey there, I'm Kamogelo Mkwanazi</h1>
-                        <p className='text-white text-xl md:text-xl pt-10'>A passionate front-end software developer using React and Node.js. 
+                        <h1 className='text-white text-3xl md:text-5xl '><MdWavingHand size={52} className='inline text-yellow-400'/> Hey there, I'm Kamogelo Mkwanazi</h1>
+                        <p className='text-white text-xl md:text-xl pt-10 mb-10'>A passionate front-end software developer using React and Node.js. 
                             Check out my projects below to see how I bring creativity and problem-solving together in code.
                         </p>
                     </div>  
@@ -31,7 +31,7 @@ export const Home = () => {
                         <p className='text-center mt-5 text-white'>Previous Projects  <FaArrowRight className='inline' /></p>
                     </Link>
                 </div>
-                <div className='container mx-auto px-4'>
+                <div className='container mx-auto px-4 mb-10'>
                     <AboutComp />
                 </div>
                 
