a00013d0457bd21a7364f617276f64717c515423
Kamogelo Mkwanazi <kamopmkwanazi@gmail.com>
2024-09-26 12:39:36 +0200

    rotate icons

--- src/Components/AboutComp.jsx
+++ src/Components/AboutComp.jsx
@@ -13,15 +13,17 @@ export const AboutComp = () => {
                 <img src={Kamo2} className='relative z-10 w-60 mb-10'></img> 
              {/* circular path for icons */}
                 <div className='absolute w-[400px] h-[400px] flex justify-center items-center border border-dashed border-pink-600 rounded-full'>
-                    <FaReact size={46} className='absolute top-0 left-1/2 transform -translate-x-1/2 -translate-y-1/2 text-blue-500 bg-white px-2 rounded-full'/>
-                    <FaNodeJs size={46} className='absolute right-0 top-1/2 transform translate-x-5 -translate-y-1/2 text-teal-500 bg-white px-2 rounded-full'/>
-                    <VscVscode size={46} className='absolute bottom-0 left-1/2 transform -translate-x-1/2 translate-y-5 text-blue-600 bg-white px-2 rounded-full'/>
-                    <FaBootstrap size={46} className='absolute left-0 top-1/2 transform -translate-x-1/2 -translate-y-1/2 text-purple-600 bg-white p-2 rounded-full'/>
+                    <div className='absolute w-full h-full animate-rotate-icons'>
+                        <FaReact size={46} className='absolute top-0 left-1/2 transform -translate-x-1/2 -translate-y-1/2 text-blue-500 bg-white px-2 rounded-full'/>
+                        <FaNodeJs size={46} className='absolute right-0 top-1/2 transform translate-x-5 -translate-y-1/2 text-teal-500 bg-white px-2 rounded-full'/>
+                        <VscVscode size={46} className='absolute bottom-0 left-1/2 transform -translate-x-1/2 translate-y-5 text-blue-600 bg-white px-2 rounded-full'/>
+                        <FaBootstrap size={46} className='absolute left-0 top-1/2 transform -translate-x-1/2 -translate-y-1/2 text-purple-600 bg-white p-2 rounded-full'/>
+                    </div>
                 </div>
                
             </div>
             <div className='flex flex-col justify-center items-center'>
-                <h3 className=' italic md:text-3xl text-purple-600 mb-8'><TfiHandPointDown className='inline '/> About ME</h3>
+                <h3 className=' italic md:text-3xl text-purple-600 mb-8'><TfiHandPointDown className='inline'/> About ME</h3>
                 <p className='text-2xl md:text-4xl mb-3'>Always Striving for Excellence</p>
                 <p className='text-xl mb-16'>With strong interpersonal-skills, clear communication and meticulous attention to detail, 
                 I am committed to delivering exceptional results in every project I take on.</p>
