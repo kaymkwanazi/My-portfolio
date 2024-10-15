22668a742e33446085d558598478ab9ba55e7535
Kamogelo Mkwanazi <kamopmkwanazi@gmail.com>
2024-10-15 11:34:21 +0200

    ..

--- src/Components/ProgressBar.jsx
+++ src/Components/ProgressBar.jsx
@@ -4,13 +4,14 @@ import 'react-circular-progressbar/dist/styles.css'
 
 export const ProgressBar = () => {
     const skills = [
-        { name: 'ReactJS', percentage: 68, color: '#264DE4' },
-        { name: 'HTML', percentage: 75, color: '#19f505' },
-        { name: 'TailwindCSS', percentage: 70, color: '#e68701' },
-        { name: 'JavaScript', percentage: 68, color: '#f505eb' },
+        { name: 'C++', percentage: 70, color: '#BC5090' },
+        { name: 'ReactJS', percentage: 60, color: '#264DE4' },
+        { name: 'HTML', percentage: 70, color: '#19f505' },
+        { name: 'TailwindCSS', percentage: 72, color: '#e68701' },
+        { name: 'JavaScript', percentage: 60, color: '#f505eb' },
         { name: 'C# Unity', percentage: 50, color: '#01dde6' },
-        { name: 'User Support', percentage: 80, color: '#f51b05' },
-        { name: 'MS 365', percentage: 80, color: '#8200ff' },
+        { name: 'User Support', percentage: 70, color: '#f51b05' },
+        { name: 'MS 365', percentage: 65, color: '#8200ff' },
     ];
 
   return (
